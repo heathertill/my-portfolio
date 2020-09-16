d8af166f27219046c770aef4645e45d0972b2cf6
Heather Till <heather.till@charter.net>
2020-09-16 10:04:57 -0500

    -getting mailgun hooked up on server side

--- src/server/routes/mailgun/index.ts
+++ src/server/routes/mailgun/index.ts
@@ -1,8 +1,8 @@
 import * as express from 'express';
-import { sendEmail } from 
+import { sendEmail } from '../../utils/mailgun/email';
 
 const router = express.Router();
-
+    
 router.post('/', async (req, res, next) => {
     try {
         await sendEmail('ktill6927@charter.net', req.body.email, req.body.subject, req.body.message);
