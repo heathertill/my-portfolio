526f00f85a7c5518864c75d0c243192aee15116f
Heather Till <heather.till@charter.net>
2020-09-23 14:48:14 -0500

    -work is fully functional, need links and text finished

--- src/client/components/public/Work.tsx
+++ src/client/components/public/Work.tsx
@@ -13,94 +13,64 @@ const Work: React.FunctionComponent<WorkProps> = () => {
         if (e) {
             setAnimate(!animate);
             if (animate !== true) {
-                expandFuncShop()
-                blogFadeOut()
-                setShowShopping(true)
+                expandFunc('.shopping', 'shop-content')
+                fadeOut('.blog')
             } else {
-                closeFuncShop()
-                blogFadeOut()
-                setShowShopping(false)
+                closeFunc('.shopping', 'shop-content')
+                fadeOut('.blog')
             }
         }
-    }
-
-    const shoppingFadeOut = () => {
-        if (animate === false) {
-            let show = document.querySelector('.shopping');
-            show.classList.add('fadeOut');
-            show.classList.remove('fadeIn')
-        } else if (animate === true) {
-            let show = document.querySelector('.shopping');
-            show.classList.add('fadeIn')
-            show.classList.remove('fadeOut')
-        }
-    }
-
-    const expandFuncShop = () => {
-        let grow = document.querySelector('.shopping');
-        grow.classList.add('toggled');
-        showDiv('shop-content');
-    }
-
-    const closeFuncShop = () => {
-        let grow = document.querySelector('.shopping');
-        grow.classList.remove('toggled');
-        hideDiv('shop-content')
-
-        // let content = document.querySelector('.work-card-content');
-        // content.classList.remove('show-content');
     }
 
     const handleBlogAnimate = (e: React.MouseEvent<HTMLDivElement>) => {
         if (e) {
             setAnimate(!animate);
             if (animate !== true) {
-                expandFuncBlog()
-                shoppingFadeOut()
-                setShowBlog(true)
+                expandFunc('.blog', 'blog-content')
+                fadeOut('.shopping')
             } else {
-                closeFuncBlog()
-                shoppingFadeOut()
-                setShowBlog(false)
+                closeFunc('.blog', 'blog-content')
+                fadeOut('.shopping')
             }
         }
+    };
+
+    const expandFunc = (growId: string, showId: string) => {
+        let grow = document.querySelector(growId);
+        grow.classList.add('toggled');
+        showDiv(showId);
     }
 
-
-
-    const showDiv = (id: string) => {
-        setTimeout(() => {
-            document.getElementById('shop-content').style.display = 'normal'
-            console.log('ding')
-        }, 2000)
+    const closeFunc = (growId: string, showId: string) => {
+        let grow = document.querySelector(growId);
+        grow.classList.remove('toggled');
+        hideDiv(showId);
     }
 
-    const hideDiv = (id: string) => {
-        setTimeout(() => {
-            document.getElementById('shop-content').style.display = 'none'
-        }, 2000)
-    }
-
-    const blogFadeOut = () => {
+    const fadeOut = (fadeId: string) => {
         if (animate === false) {
-            let show = document.querySelector('.blog');
+            let show = document.querySelector(fadeId);
             show.classList.add('fadeOut');
             show.classList.remove('fadeIn')
         } else if (animate === true) {
-            let show = document.querySelector('.blog');
+            let show = document.querySelector(fadeId);
             show.classList.add('fadeIn')
             show.classList.remove('fadeOut')
         }
     }
 
-    const expandFuncBlog = () => {
-        let grow = document.querySelector('.blog');
-        grow.classList.add('toggled');
-    }
 
-    const closeFuncBlog = () => {
-        let grow = document.querySelector('.blog');
-        grow.classList.remove('toggled');
+    const showDiv = (id: string) => {
+        setTimeout(() => {
+            document.getElementById(id).classList.add('showDiv');
+            document.getElementById(id).classList.remove('hideDiv')
+        }, 1500)
+    }
+    const hideDiv = (id: string) => {
+        setTimeout(() => {
+            document.getElementById(id).classList.remove('showDiv')
+            document.getElementById(id).classList.add('hideDiv');
+        }, 500)
     }
 
     const openApp = (id: string) => {
@@ -109,22 +79,19 @@ const Work: React.FunctionComponent<WorkProps> = () => {
 
     return (
         <section className="work-section">
-            {/* <img src="images/codeSample.png" alt="computer code image" className="work-background" /> */}
             <img src="images/codeScreen.png" alt="computer code image" className="work-background" />
-
             <div className="work-over">
                 <div className="work-intro">
                     <h1 className="work-title">Check out some of my work...</h1>
                 </div>
-                <div>
-                <div className="row-cols-2 row-cols-sm-1 d-flex justify-content-center row work-body">
+                <div className="row row-cols-2 row-cols-sm-1 d-flex justify-content-center">
                     <div className="col-md-4 work-card-div">
                         <div className="card work-card shopping"
                             onClick={(e: React.MouseEvent<HTMLDivElement>) => handleShoppingAnimate(e)} >
-                            <img src="images/shopComp.png" alt="shoping app" className="card-img card-img-top" />
+                            <img src="images/shopComp.png" alt="shoping app" className="card-img-top" />
                             <div className="card-title work-card-title">Shopping</div>
-                            <div className="work-card-content" id="shop-content">
-                                <div>This is a fullstack shopping app. Users login and add items to the group shopping list. When items are purchased or removed the user who added the item will recieve a text informing them of the items status.</div>
+                            <div className="card-text hideDiv" id="shop-content">
+                                <div className="text-myWhite">This is a fullstack shopping app. Users login and add items to the group shopping list. When items are purchased or removed the user who added the item will recieve a text informing them of the items status.</div>
                                 <a href="https://stormy-sea-01231.herokuapp.com">See it working!</a>
                                 <button onClick={() => openApp("https://stormy-sea-01231.herokuapp.com")}>See it working!</button>
                             </div>
@@ -132,18 +99,15 @@ const Work: React.FunctionComponent<WorkProps> = () => {
                     </div>
                     <div className="col-md-4 offset-md-1 work-card-div" id="blog">
                         <div className="card work-card blog"
-                            onClick={(e: React.MouseEvent<HTMLDivElement>) => handleBlogAnimate(e)}
-                        >
+                            onClick={(e: React.MouseEvent<HTMLDivElement>) => handleBlogAnimate(e)} >
                             <img src="images/blogComp.png" alt="blog app" className="card-img card-img-top" />
                             <div className="card-title work-card-title">Blog</div>
-                            <div className="card-text">
-                                Stuff about the app
+                            <div className="hideDiv" id="blog-content">
+                                <div className="card-text">Stuff about the app</div>
+                                <button className="" onClick={() => openApp("https://stormy-sea-01231.herokuapp.com")}>See it working!</button>
                             </div>
-                            <button onClick={() => openApp("https://stormy-sea-01231.herokuapp.com")}>See it working!</button>
-
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </section>
